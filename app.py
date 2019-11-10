7a1ab676b2cf7c06c390ee2b48031e575556a2fa
Ted Haley <ted.haley@berlinguettegroup.com>
2019-11-09 19:17:46 -0800

    added stuff

--- app.py
+++ app.py
@@ -10,6 +10,8 @@ import dash_bootstrap_components as dbc
 from dash.dependencies import Input, Output
 import plotly.graph_objects as go
 from pprint import pprint
+import plotly.express as px
+import random
 
 streetlight_df = pd.read_csv('/Users/teddyhaley/PycharmProjects/DeCodeCongestion/data/street-lighting-poles.csv',
                              sep=';')
@@ -25,7 +27,7 @@ streetlight_df['distance_centre_norm'] = 1 - ((streetlight_df['distance_centre']
     streetlight_df['distance_centre'])) / (max(streetlight_df['distance_centre']) - min(
     streetlight_df['distance_centre'])))
 
-external_stylesheets = [dbc.themes.FLATLY]
+external_stylesheets = [dbc.themes.YETI]
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 server = app.server
@@ -38,17 +40,12 @@ mapbox_access_token = "pk.eyJ1IjoicGxvdGx5bWFwYm94IiwiYSI6ImNqdnBvNDMyaTAxYzkzeW
 
 logo = html.A(
     # Use row and col to control vertical alignment of logo / brand
-    dbc.Row(
-        [
-            dbc.Col(
-                html.Img(
-                    src=app.get_asset_url('text_logo.png'),
-                    height="60px",
-                    style={'marginRight': '1em'})),
-        ],
-        align="center",
-        no_gutters=True,
-    ),
+    children=[
+        html.Img(
+            src=app.get_asset_url('text_logo.png'),
+            height="60px",
+            style={'marginRight': '1em'})
+    ],
     href="https://github.com/TedHaley/DeCodeCongestion",
     target='_blank'
 )
@@ -58,11 +55,14 @@ navbar = dbc.Navbar(
         dbc.Row(
             [
                 dbc.Col(logo,
-                        width={"size": 2, "offset": 0, 'order': 1}),
+                        width={"size": 2, "offset": 0, 'order': 1}
+                        ),
 
                 dbc.Col(
                     html.H1('LIGHT SPEED',
-                            style={'color': 'white'})
+                            style={'color': 'white'}
+                            ),
+                    width={"size": 3, "order": "last", "offset": 0},
                 ),
 
             ],
@@ -71,7 +71,6 @@ navbar = dbc.Navbar(
         ),
     ],
     color="primary",
-    dark=True,
     style={'marginBottom': '1em'}
 )
 
@@ -91,14 +90,16 @@ body = html.Div(
         html.Div(
             children=[
                 dcc.Graph(id="map-graph",
-                          style={'height': '70vh'}
+                          style={'height': '75vh'}
                           ),
             ],
-            style={'height': '70vh',
+            style={'height': '75vh',
                    'marginBottom': '1em'}
         ),
 
         html.Div(id='data_table'),
+
+        html.Div(id='polar_plot'),
 
         dcc.Interval(
             id='interval-component',
@@ -139,8 +140,6 @@ def update_metrics(n):
               [Input('interval-map', 'n_intervals'),
                Input('time-slider', 'value')])
 def update_map(n, time_value):
-    now = datetime.datetime.now()
-
     streetlight_df['time_norm'] = time_value / 24
     streetlight_df['cos_time'] = streetlight_df['time_norm'].apply(lambda x: (math.cos(x * 2 * math.pi) * -2))
     streetlight_df['time_intensity'] = streetlight_df['distance_centre_norm'] * streetlight_df['cos_time']
@@ -159,6 +158,8 @@ def update_map(n, time_value):
                       mapbox_zoom=11
                       )
 
+    fig.update_layout(showlegend=False)
+
     fig.update_layout(margin={"r": 0, "t": 0, "l": 0, "b": 0})
 
     return fig
@@ -171,20 +172,74 @@ def em_optimizer_parameters(clickData):
     if clickData is not None:
         df = streetlight_df[streetlight_df['node_number'] == clickData['points'][0]['pointNumber']]
 
-        print(df.columns)
-
         new_df = pd.DataFrame()
         new_df["Node"] = df['node_number']
         new_df['Neighbourhood'] = df['Geo Local Area']
         new_df['Latitude'] = df['lat']
         new_df['Longitude'] = df['lng']
         new_df['Distance Center'] = df['distance_centre_norm']
-        # new_df['Devices in Network'] = df['time_intensity_norm'] + 5
+        new_df['Devices in Network'] = df['time_intensity_norm'] * 50
+        new_df['Devices in Network'] = new_df['Devices in Network'].apply(lambda x: int(x))
 
-        return dbc.Table.from_dataframe(new_df, striped=True, bordered=True, hover=True)
+        div = [html.H4('Node Information:'),
+               dbc.Table.from_dataframe(new_df, striped=True, bordered=True, hover=True)]
+
+        return div
 
 
-# table = dbc.Table.from_dataframe(df, striped=True, bordered=True, hover=True)
+@app.callback(
+    Output('polar_plot', 'children'),
+    [Input('map-graph', 'clickData'),
+     Input('interval-map', 'n_intervals')])
+def polar_chart_update(clickData, n):
+    if clickData is not None:
+
+        point = clickData['points'][0]['pointNumber']
+        df = streetlight_df[streetlight_df['node_number'] == point]
+        devices = int(df.iloc[0]['time_intensity_norm'] * 50)
+
+        dist_list = []
+        angle_list = []
+
+        for i in range(0, devices):
+            x = random.randint(1, 50)
+            dist_list.append(x)
+
+        for i in range(0, devices):
+            x = random.randint(0, 15)
+            dir_list = ['N', 'NNE', 'NE', 'ENE', 'E', 'ESE', 'SE', 'SSE', 'S', 'SSW', 'SW', 'WSW', 'W', 'WNW', 'NW',
+                        'NNW']
+            angle = dir_list[x]
+            angle_list.append(angle)
+
+        df_new = pd.DataFrame()
+        df_new['User'] = np.arange(devices)
+        df_new['Distance From Node (m)'] = dist_list
+        df_new['Direction'] = angle_list
+
+        fig = px.scatter_polar(df_new, r="Distance From Node (m)", theta="Direction", title=f"Devices Around Node: {point}")
+        polar_graph = dcc.Graph(figure=fig)
+
+
+
+        div = dbc.Row(
+            [
+                dbc.Col(
+                    polar_graph,
+                    md=8
+                ),
+
+                dbc.Col(
+                    dbc.Table.from_dataframe(df_new, striped=True, bordered=True, hover=True),
+                    md=4
+                ),
+
+            ],
+            style={"width": "100%"},
+            # align='center'
+        )
+
+        return div
 
 
 if __name__ == '__main__':
